68d5dc6df859e85e008e83d01bdd66b7aa2a86fa
Ben Simpson <bmsr56@mst.edu>
2018-10-01 20:55:58 -0500

    shiz

--- py_code/bluetooth/bluetooth.py
+++ py_code/bluetooth/bluetooth.py
@@ -1,34 +1,36 @@
 import bluetooth
-import RPi.gpio as gpio        #calling for header file which helps in using gpios of PI
-from functions.functions import * as fn
-LED=21
+import RPi.GPIO as gpio        #calling for header file which helps in using gpios of PI
+from functions.functions import *
 
+# Set GPIO settings here
 gpio.setmode(gpio.BCM)     #programming the gpio by BCM pin numbers. (like PIN40 as gpio21)
 gpio.setwarnings(False)
-gpio.setup(LED,gpio.OUT)  #initialize gpio21 (LED) as an output Pin
-gpio.output(LED,0)
 
-server_socket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+# Set outputs here
 
-port = 1
-server_socket.bind(("",port))
-server_socket.listen(1)
-print 'Waiting for connection...'
-client_socket,address = server_socket.accept()
-print "Accepted connection from ",address
-while 1:
+# Initialize outputs here
 
- data = client_socket.recv(1024)
- print "Received: %s" % data
- if (data == "0"):    #if '0' is sent from the Android App, turn OFF the LED
-  print ("gpio 21 LOW, LED OFF")
-  gpio.output(LED,0)
- if (data == "1"):    #if '1' is sent from the Android App, turn OFF the LED
-  print ("gpio 21 HIGH, LED ON")
-  gpio.output(LED,1)
- if (data == "q"):
-  print ("Quit")
-  break
+def connectBluetooth(output):
+  server_socket = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  port = 1
+  server_socket.bind(("",port))
+  server_socket.listen(1)
+  print('Waiting for connection...')
+  client_socket,address = server_socket.accept()
+  print('Accepted connection from ', address)
+  while 1:
+    data = client_socket.recv(1024)
+    print ("Received: %s" % data)
+    if (data == "0"):    #if '0' is sent from the Android App, turn OFF the LED
+      print ("gpio 21 LOW, LED OFF")
+      gpio.output(output,0)
+    if (data == "1"):    #if '1' is sent from the Android App, turn OFF the output
+      print ("gpio 21 HIGH, output ON")
+      gpio.output(output,1)
+    if (data == "q"):
+      print ("Quit")
+      break
 
-client_socket.close()
-server_socket.close()
+  client_socket.close()
+  server_socket.close()
+  return
--- py_code/main.py
+++ py_code/main.py
@@ -1,18 +1,18 @@
-from bluetooth.bluetooth import * as bt
-from pump.pump import * as pump
-from functions.functions import * as fn
-import RPi.gpio as gpio
+from bluetooth.bluetooth import *
+from pump.pump import *
+from functions.functions import *
+import RPi.GPIO as gpio
 
 def main():
     try:
-    outputPins = [4]
-    fn.setAsOutputs(outputPins)
-    fn.setOutputValues(0, outputPins)
-    pump = pump.assignPumps(4)
+        outputPins = [4]
+        fn.setAsOutputs(outputPins)
+        fn.setOutputValues(0, outputPins)
+        pumps = pump.assignPumps(4)
 
-    while True:
-        input()
-        fn.gpioRun(pump[0], 3)
+        while True:
+            input()
+            fn.gpioRun(pumps[0], 3)
     
     except KeyboardInterrupt:
         gpio.cleanup()
