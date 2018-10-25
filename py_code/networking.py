8a371cbc9f4d6f5c499b3d74ca889db91089d6f9
Ben Simpson <bmsr56@mst.edu>
2018-10-25 18:33:53 -0500

    changed netowrking to use pyrebase

--- py_code/networking.py
+++ py_code/networking.py
@@ -1,6 +1,16 @@
+
 from functions import *
 import RPi.GPIO as gpio
-import pyrebase, pyfcm
+import pyrebase
+# from firebase_admin import credentials
+# from firebase_admin import db
+
+
+# cred = credentials.Certificate('drinkmasterplus-ec13a-firebase-adminsdk-0ek53-cd1c117f9d.json')
+# default_app = firebase_admin.initialize_app(cred, {
+#     'databaseURL': 'https://drinkmasterplus-ec13a.firebaseio.com'
+#     })
+
 
 # https://www.raspberrypi.org/documentation/configuration/security.md
 
@@ -26,15 +36,18 @@ password = "thisisapassword"
 
 # Get an instance
 firebase = pyrebase.initialize_app(config)
-
+print('got instance')
 # Get a reference to the auth service
 auth = firebase.auth()
+print('auth ref')
 
 # Log the user in
 user = auth.sign_in_with_email_and_password(email, password)
+print('authd')
 
 # Get a reference to the database service
 db = firebase.database()
+print('db ref')
 
 # data to save
 data = {
@@ -43,3 +56,5 @@ data = {
 
 # Pass the user's idToken to the push method
 results = db.child("accounts").get()
+print(results)
+print('got accounts')
