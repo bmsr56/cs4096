49b0aa5306a2c3842735117d300f3edcf092e528
Justin M Spencer <justinmspencer@r02azsxt5.device.mst.edu>
2018-11-05 16:53:24 -0600

    made cookbook recycler adaptor

---
+++ AutomaticAlcohol/app/src/main/java/com/example/nathan/automaticalcohol/Adapters/CookbookResultsRecyclerAdapter.java
@@ -0,0 +1,14 @@
+package com.example.nathan.automaticalcohol.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+public class CookbookResultsRecyclerAdapter extends RecyclerView.Adapter<SpecialsRecyclerAdapter.MyViewHolder> {
+
+    private static final String TAG = "CookbookResultsRecyclerAdapter";
+
+
+
+
+
+
+}
