6f475adbf59ccf861bc2b17ea0cad9ce6317149e
Ben Simpson <bmsr56@mst.edu>
2018-11-05 18:39:33 -0600

    testing networking fns

--- py_code/networking.py
+++ py_code/networking.py
@@ -1,60 +1,36 @@
-
 from functions import *
 import RPi.GPIO as gpio
 import pyrebase
-# from firebase_admin import credentials
-# from firebase_admin import db
 
+def connectFB(email, password):
+    config = {
+        "apiKey": "AIzaSyDoofjCqY0kx9IPrjwY0ZD_RaXuqY4kQ5k",
+        "authDomain": "drinkmasterplus-ec13a.firebaseapp.com",
+        "databaseURL": "https://drinkmasterplus-ec13a.firebaseio.com",
+        "projectId": "drinkmasterplus-ec13a",
+        "storageBucket": "drinkmasterplus-ec13a.appspot.com"
+        # messagingSenderId: "1027801399754"
+    }
 
-# cred = credentials.Certificate('drinkmasterplus-ec13a-firebase-adminsdk-0ek53-cd1c117f9d.json')
-# default_app = firebase_admin.initialize_app(cred, {
-#     'databaseURL': 'https://drinkmasterplus-ec13a.firebaseio.com'
-#     })
+    # Get an instance
+    print('Obtaining Firebase instance...')
+    firebase = pyrebase.initialize_app(config)
+    print('Firebase instance obtained')
 
-# https://www.raspberrypi.org/documentation/configuration/security.md
+    # Get a reference to the auth service
+    print('Obtaining Auth reference...')    
+    auth = firebase.auth()
+    print('Auth reference obtained')
 
-# Make Pi an access point
-# Manage security rules
-# Other network related functions
+    # Log the user in
+    print('Logging {} in...'.format(email))
+    user = auth.sign_in_with_email_and_password(email, password)
+    print('{} logged in'.format(email))
 
-# Authenticate with Firebase DB
-
-
-config = {
-    "apiKey": "AIzaSyDoofjCqY0kx9IPrjwY0ZD_RaXuqY4kQ5k",
-    "authDomain": "drinkmasterplus-ec13a.firebaseapp.com",
-    "databaseURL": "https://drinkmasterplus-ec13a.firebaseio.com",
-    "projectId": "drinkmasterplus-ec13a",
-    "storageBucket": "drinkmasterplus-ec13a.appspot.com"
-    # messagingSenderId: "1027801399754"
-}
-
-# User credentials
-email = "DrinkMasterPlusPlus@gmail.com"
-password = "thisisapassword"
-
-# Get an instance
-firebase = pyrebase.initialize_app(config)
-print('got instance')
-# Get a reference to the auth service
-auth = firebase.auth()
-print('auth ref')
-
-# Log the user in
-user = auth.sign_in_with_email_and_password(email, password)
-print('authd')
-
-# Get a reference to the database service
-db = firebase.database()
-print('db ref')
-
-# data to save
-data = {
-    "name": "Drinky drinkerson"
-}
-
-# Pass the user's idToken to the push method
-results = db.child("accounts").get()
-print(results)
-print('got accounts')
+    # Get a reference to the database service
+    print('Obtaining Firebase reference...')
+    db = firebase.database()
+    print('Firebase reference obtained')
+    
+    return db, user
 
