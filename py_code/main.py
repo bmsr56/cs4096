95e6fe0444f3ea967a3e89f08e154ac1fe5a7575
Ben Simpson <bmsr56@mst.edu>
2018-12-04 01:02:50 -0600

    updates to main and threading

--- py_code/main.py
+++ py_code/main.py
@@ -2,20 +2,44 @@ from pump import *
 from functions import *
 from networking import *
 import RPi.GPIO as gpio
+import threading
 
-def processQueue():
-    pass
-    # handle garbage
-    # LOOP: check length of drinkQueue and process one at a time
-        # extract drink string
-        # call parser on the string
-        # process each ingredient with calls to threaded pumprun fns
+drinkProcessedFlag = False
+pumps = {
+    '1': 10, 
+    '2': 9, 
+    '3': 11, 
+    '4': 8 
+}
+pumpPins = [10, 9, 11, 8]
+pump1 = 10
+pump2 = 9
+pump3 = 11
+pump4 = 8
 
-    # if len(drinkQueue) > 0:
-    #     if drinkFinished is True:
+def mlToSeconds(ml):
+    ml = float(ml)
+    rate = 44 / 30
+    return (ml / rate)
 
+def processQueueItem():
+    """Items stored in the drinkQueue consist of specially formatted strings
+    """
+    # handle garbage that might be in there at first
+    # extract drink string
+    # call parser on the string
+    pumpNumbers, amounts = parser(drinkQueue[0]) # this is the first item in the drink queue
+    
 
-    # return  
+    # process each ingredient with calls to threaded pumprun fns
+    if len(pumpNumbers) == len(amounts): # this should always be true... just a check
+        for i in range(len(pumpNumbers)):
+            # run pumps with threading
+            threading.Thread(target=gpioRun, args=[]).start()            
+    # remove the just processed item from the queue
+    # set flag to True
+    
+    return  
 
 def parser(msg):
     pumpNumbers = []
@@ -28,13 +52,9 @@ def parser(msg):
     return pumpNumbers, amounts
 
 def main():
-    pumpPins = [10, 9, 11, 8]
+    
     setAsOutput(pumpPins)
     setOutputValue(1, pumpPins)
-    pump1 = 10
-    pump2 = 9
-    pump3 = 11
-    pump4 = 8
 
     try:
         db, user = connectFB("DrinkMasterPlusPlus@gmail.com", "thisisapassword")
@@ -47,6 +67,11 @@ def main():
 
         print('STREAM TYPE', type(stream))
         print('DRINKQ', drinkQueue)
+
+        while len(drinkQueue) > 0 and drinkProcessedFlag is False:
+            processQueueItem()
+
+
         # the schema of the database was changed so this had to change
         # db.child("loadout").child("1").set({"amountLeft": 9000})
         # db.child("loadout").child("1").set({"bottleName": "poojuice"})
