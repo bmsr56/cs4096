ef13d548708b16c7a3bc4c954902758d3291e361
Ben Simpson <bmsr56@mst.edu>
2018-12-04 03:10:09 -0600

    syntax

--- py_code/testpump.py
+++ py_code/testpump.py
@@ -5,34 +5,15 @@ import RPi.GPIO as gpio
 import threading
 
 def main():
-    try:
-        db, user = connectFB("DrinkMasterPlusPlus@gmail.com", "thisisapassword")
-        # loadout = db.child("loadout").get()
-        # for item in loadout.each():
-        #     print(item.key())
-        #     print(item.val())
-    
-        # stream = db.child("queue").stream(queue_handler)
-
-        # print('STREAM TYPE', type(stream))
-
-        # while 1:
-        #     if currentPath is not None:
-        #         db.child("queue").child(currentPath).remove()
-        #         currentPath = None
-        #     time.sleep(1)
-
-        # the schema of the database was changed so this had to change
-        db.child("queue").push("1_50.0+3_50.0")
-        # db.child("loadout").child("1").set({"bottleName": "poojuice"})
-        # db.child("loadout").child("2").set({"amountLeft": 900000000000})
-        # db.child("loadout").child("2").set({"bottleName": "poojuice2"})
-		
+	try:
+		db, user = connectFB("DrinkMasterPlusPlus@gmail.com", "thisisapassword")
+		db.child("queue").push("1_50.0+3_50.0")
 	except KeyboardInterrupt:
-        stream.close()
-    finally:
-        gpio.cleanup()
-    return
+		# stream.close()
+		pass
+	finally:
+		gpio.cleanup()
+	return
 
 
 if __name__ == '__main__':
