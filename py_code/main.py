e3316fcb2f220b33de9728b14c925f13db2f0861
Ben Simpson <bmsr56@mst.edu>
2018-12-03 20:10:27 -0600

    added bens main.py

--- py_code/main.py
+++ py_code/main.py
@@ -1,51 +1,27 @@
-from bt import *
 from pump import *
 from functions import *
-import RPi.GPIO as gpio, pyrebase
+from networking import *
+import RPi.GPIO as gpio
 
 def main():
     try:
-        config = {
-            "apiKey": "AIzaSyDoofjCqY0kx9IPrjwY0ZD_RaXuqY4kQ5k",
-            "authDomain": "drinkmasterplus-ec13a.firebaseapp.com",
-            "databaseURL": "https://drinkmasterplus-ec13a.firebaseio.com/",
-            "storageBucket": "gs://drinkmasterplus-ec13a.appspot.com"
-            }
+        db, user = connectFB("DrinkMasterPlusPlus@gmail.com", "thisisapassword")
+        # loadout = db.child("loadout").get()
+        # for item in loadout.each():
+        #     print(item.key())
+        #     print(item.val())
 
-        firebase = pyrebase.initialize_app(config)
+        loadout_stream = db.child("loadout").stream(sh_loadout)
 
-        # Get a reference to the auth service
-        auth = firebase.auth()
+        # the schema of the database was changed so this had to change
+        db.child("loadout").child("1").set({"amountLeft": 9000})
+        db.child("loadout").child("1").set({"bottleName": "poojuice"})
+        db.child("loadout").child("2").set({"amountLeft": 900000000000})
+        db.child("loadout").child("2").set({"bottleName": "poojuice2"})
 
-        # Log the user in
-        user = auth.sign_in_with_email_and_password(email, password)
-
-        # Get a reference to the database service
-        db = firebase.database()
-
-        # data to save
-        data = {
-            "name": "Mortimer 'Morty' Smith"
-        }
-
-        # Pass the user's idToken to the push method
-        results = db.child("users").push(data, user['idToken'])
-
-        outputPins = [4, 17, 27, 22, 5, 6]
-        setAsOutput(outputPins)
-        setOutputValue(0, outputPins)
-        
-        # pumps is a dictionary <int>:<int>:
-        # e.g. {0 : pin1, 1 : pin2 ... 5 : pin5}
-        pumps = assignPumps(outputPins)
-
-        while True:
-            input()
-            gpioRun(pumps[0], 3)
-    
-    except KeyboardInterrupt:
+    finally:
         gpio.cleanup()
-        
+        loadout_stream.close()
     return
 
 if __name__ == '__main__':
